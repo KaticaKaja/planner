00acaabc1d2f5e10917ae0350553a073d31e1502
Katica Rankovic <katica.r@outlook.com>
2024-02-28 11:38:53 +0100

    loader

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ import './src/core/router.js';
 import './src/core/domchanges.js';
 
 DB.init();
+
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(() => {
+        var loadingOverlay = document.getElementById('loading');
+        loadingOverlay.style.display = 'none';
+        document.querySelector('header').style.visibility = 'visible';
+        document.querySelector('main').style.visibility = 'visible';
+        document.querySelector('main').style.opacity = '1';
+        document.querySelector('footer').style.visibility = 'visible';
+        document.querySelector('footer').style.transform = "translateY(0)";
+    }, 500);
+});
